1fd2cbba1bca51e3f6f94219285a201aff4f0796
Jimboni-tech <yunchengzhou@gmail.com>
2024-10-22 12:10:06 -0500

    Created basic Sentiment analysis file and started scraper file

--- sentiment.py
+++ sentiment.py
@@ -1,14 +1,5 @@
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-analyzer = SentimentIntensityAnalyzer()
-text = "I really enjoyed this book!"
-scores = analyzer.polarity_scores(text)
-print(scores)
 
-
-from textblob import TextBlob
-
-text = "I really enjoyed this book!"
-blob = TextBlob(text)
-sentiment = blob.sentiment
-print(sentiment) 
+tweet = 'Skillcate is a great Youtube Channel to learn Data Science'
+print(SentimentIntensityAnalyzer().polarity_scores(tweet))
