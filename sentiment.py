d34152fcb148d5aea1162282ed3e60a437011174
Jimboni-tech <yunchengzhou@gmail.com>
2025-01-27 22:47:29 -0600

    Added graphing functionality as well as cleaning and sorting text.

--- sentiment.py
+++ sentiment.py
@@ -1,28 +1,55 @@
 import praw
-from IPython import display
-import math
-from pprint import pprint
 import pandas as pd
 import numpy as np
-import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set(style='darkgrid', context='talk', palette='Dark2')
-plt.style.use('ggplot')
-reddit = praw.Reddit(
-    client_id="_pKCPsBoDa1S3pYDVmeGqQ",
-    client_secret="toDp5RAErDA6KL_7zzUJiu4KT-wjuA",
-    password="1playclarinetF",
-    user_agent="Cloud-General",
-    username="Cloud-General",
-)
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
-data = set()
+
+
+
 sia = SIA()
+
+subreddit_string = (
+    'crypto+CryptoCurrency+CryptoMarkets+CryptoMoonShots+Crypto_com+Bitcoin+CryptoTechnology+'
+    'CryptoMars+btc+CryptoCurrencyTrading+CryptoMoon+Crypto_Currency_News+Crypto_General+ethereum+'
+    'blockchain+Cryptocurrency+cryptonews+Cryptos+DeFi+Litecoin+dogecoin+XRP+Solana+Altcoin+NFTs+cryptotokens'
+    '+cryptoinvesting+cryptotrading+blockchainnews+cryptocurrencynews+cryptomarket'
+)
+
+
+def preprocess_text(text):
+    text = text.lower()
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word.isalpha() and word not in stop_words]
+    ps = PorterStemmer()
+    tokens = [ps.stem(word) for word in tokens]
+    return tokens
+
+
+def label_sentiment(row):
+    if row['compound'] >= 0.05:
+        return 'Positive'
+    elif row['compound'] <= -0.05:
+        return 'Negative'
+    else:
+        return 'Neutral'
+
+
+def clean_and_sort_data(df):
+    df = df[df['Upvotes'] > 0.2]  
+    df = df[df['Text'].str.len() > 50]
+    df = df[abs(df['compound']) < 0.9] 
+    df = df.drop_duplicates(subset='Headline', keep='first') 
+    df = df.sort_values(by='Upvotes', ascending=False)  
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
 rawData = []
 keywordList = '"Bitcoin", "$BTC", "BTC"'
-subreddit = reddit.subreddit('crypto+CryptoCurrency+CryptoMarkets+CryptoMoonShots+Crypto_com+Bitcoin+CryptoTechnology+CryptoMars+btc+CryptoCurrencyTrading+CryptoMoon+Crypto_Currency_News+Crypto_General').search(keywordList, limit=None, time_filter='day')
-subreddit2 = reddit.subreddit('all').search(keywordList, limit=None, time_filter='day')
+subreddit = reddit.subreddit(subreddit_string).search(keywordList, limit=None, time_filter='day')
+
 for submission in subreddit:
     if not submission.stickied:
         if len(submission.selftext) > 0:
@@ -33,11 +60,23 @@ for submission in subreddit:
             pol_score['Upvotes'] = submission.upvote_ratio
             rawData.append(pol_score)
 
+
 df = pd.DataFrame.from_records(rawData)
-display.display(df)
-print(len(df))
 
-df.to_csv('10-30data.csv', header = False, encoding = "utf-8", index = False)
-df2 = df[['Headline', 'compound']]
-df2.to_csv('test.csv', header = False, encoding = "utf-8", index = False)
 
+df_cleaned = clean_and_sort_data(df)
+
+
+df_cleaned['Sentiment Label'] = df_cleaned.apply(label_sentiment, axis=1)
+
+print(df_cleaned.head())
+
+plt.figure(figsize=(8, 6))
+sns.countplot(x='Sentiment Label', data=df_cleaned, palette='Set2')
+plt.title('Sentiment Distribution of Posts')
+plt.xlabel('Sentiment')
+plt.ylabel('Count')
+plt.show()
+
+
+df_cleaned.to_csv('wkOfJan27Data.csv', header=True, encoding="utf-8", index=False)
